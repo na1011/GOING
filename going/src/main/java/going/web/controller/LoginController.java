72e38b3d432255ff58e245ba35d7a4f32b59a406
KYS <support@webisfree.com>
2023-11-16 17:42:35 +0900

    kys

--- going/src/main/java/going/web/controller/LoginController.java
+++ going/src/main/java/going/web/controller/LoginController.java
@@ -9,14 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
-public class HomeController extends HttpServlet {
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-        String viewPath = "/WEB-INF/view/login.jsp";
-        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        String Login = "/WEB-INF/view/login.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(Login);
         dispatcher.forward(req, resp);
 	}
 	
