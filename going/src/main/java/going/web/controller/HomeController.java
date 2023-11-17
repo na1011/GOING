0213b5424e16a167ff2a5d9e1ee0e30d62aefdff
YWJ <ywj184077@naver.com>
2023-11-17 15:08:19 +0900

    ywj

--- going/src/main/java/going/web/controller/HomeController.java
+++ going/src/main/java/going/web/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-        String viewPath = "/WEB-INF/view/index.jsp";
+        String viewPath = "/WEB-INF/member/Sign_up.jsp";
         RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
         dispatcher.forward(req, resp);
 	}
